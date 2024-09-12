2b83fb97fde52ddf82b1bd95a0a0818c577877b2
Vinicius Higa <viniciushigadev@gmail.com>
2024-09-11 22:37:54 -0300

    [ADD] - Simple upload function

--- cmd/main.go
+++ cmd/main.go
@@ -8,11 +8,16 @@ import (
 
 func main() {
 	var server *gin.Engine = gin.Default()
+	server.MaxMultipartMemory = 8 << 20
+
+	server.POST("/upload", func(context *gin.Context) {
+		file, _ := context.FormFile("file")
+		context.SaveUploadedFile(file, "../tests/"+file.Filename)
+		context.JSON(http.StatusOK, gin.H{})
+	})
 
 	server.GET("/download", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		context.JSON(http.StatusOK, gin.H{})
 	})
 
 	server.RunTLS(":8080", "server.pem", "server.key")
